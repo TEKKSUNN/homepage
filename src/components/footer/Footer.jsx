a6c0955b5089d9457dea95e0cdde52b906b27555
TEKKSUNN <emmanuelleutecson@gmail.com>
2025-01-06 20:29:59 +0800

    feat: Make Footer & style it

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,26 @@
+import './Footer.css';
+import { SocialButtons } from '../buttons/Buttons.jsx';
+import Phone from '../../assets/phone.svg';
+import Email from '../../assets/email.svg';
+
+function Footer() {
+  return (
+    <footer>
+      <h1 className="title">Contact Me</h1>
+      <p className="desc">Please get in touch with us if you think our work could be mutually beneficial.</p>
+      <p className="address">1234 Random Road<br />Random Town, Narnia 12345</p>
+      <div className="phone info">
+        <img src={Phone} alt="phone icon" />
+        <p className="icon-desc">123-456-7890</p>
+      </div>
+      <div className="email info">
+        <img src={Email} alt="email icon" />
+        <p className="icon-desc">emmanuelleutecson@gmail.com</p>
+      </div>
+      <SocialButtons buttonClassName="footer-button" />
+    </footer>
+  );
+}
+
+export default Footer;
+
